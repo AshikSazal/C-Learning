a2960fe48e2b743da2c7fbff7688b0f67b249519
Ashik Sazal <sojolislm@gmail.com>
2023-03-08 16:15:39 +0600

    first commit

---
+++ 2. File create current directory/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Example
+{
+    class Program1
+    {
+        static void Main(String[] args)
+        {
+            string path = Environment.CurrentDirectory + "\\Example.txt";
+            System.Diagnostics.Debug.WriteLine(path);
+            if(!File.Exists(path))
+            {
+                File.Create(path);
+            }
+        }
+    }
+}
