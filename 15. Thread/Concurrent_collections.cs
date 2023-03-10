2c433de923128f753461931d2a6040afed7e6103
Ashik Sazal <sojolislm@gmail.com>
2023-03-10 11:08:36 +0600

    first commit

---
+++ 15. Thread/Concurrent_collections.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            List<Task> tasks = new List<Task>();
+            for(int i=0; i<100; i++)
+            {
+                var numberToAdd = i;
+                tasks.Add(Task.Run(()=> bag.Add(numberToAdd)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            List<Task> runningTasks = new List<Task>();
+            int numberOfItems = 0;
+            while (!bag.IsEmpty)
+            {
+                runningTasks.Add(Task.Run(() =>
+                {
+                    int item;
+                    if (bag.TryTake(out item))
+                    {
+                        Debug.WriteLine(item);
+                        numberOfItems++;
+                    }
+                }));
+            }
+
+            Task.WaitAll(runningTasks.ToArray());
+
+            Debug.WriteLine($"There were {numberOfItems} items in the bag");
+        }
+    }
+}
