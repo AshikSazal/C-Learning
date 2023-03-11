0b77aa6463e5a6f9b30893c82ed9e2c507617c47
Ashik Sazal <sojolislm@gmail.com>
2023-03-11 15:21:19 +0600

    first commit

---
+++ 20. Database/1. Connection_&_Select.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=ASHIK\SQLEXPRESS;
+                                        Initial Catalog=Example;
+                                        User ID=USER;
+                                        password=;
+                                        Trusted_Connection=Yes;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Debug.WriteLine("Connected to the Server!");
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT * FROM dbo.People";
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    Debug.WriteLine(reader.GetString(1) + "-" + reader.GetString(2));
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
